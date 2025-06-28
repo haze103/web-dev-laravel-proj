d9725fb5bc37de2ab013dd1dfe3895f3bc9a6785
veenoise <william.eduard123@gmail.com>
2025-06-28 15:51:47 +0800

    Add: migrations

--- database/migrations/2025_06_24_061715_create_leads_table.php
+++ database/migrations/2025_06_24_061715_create_leads_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,14 @@ return new class extends Migration
         Schema::create('leads', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('company');
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('assigned_to')->constrained('users')->nullable();
+            $table->enum('stage', ['new', 'contacted', 'proposal sent', 'won', 'lost'])->constrained('users');
+            $table->date('closing_date');
+            $table->decimal('amount', 18, 2);
+            $table->enum('status', ['active', 'follow-up', 'cold']);
         });
     }
 
---
+++ database/migrations/2025_06_28_074043_create_contacts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone_number', 30);
+            $table->string('company');
+            $table->string('position');
+            $table->foreignId('sales_representative_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
