a66208fcd5e1bdb4bab1dba8352c299c4d1a6701
Sybil <oui.cruzado@gmail.com>
2025-06-29 19:56:58 +0800

    Add role seeder and assign roles to users

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -6,13 +6,17 @@ class RoleSeeder extends Seeder
 {
     public function run()
     {
+        // Define a list of roles to be created in the system
         $roles = ['Super Admin', 'Admin', 'Sales Manager', 'Sales Representative'];
 
+        // Loop through each role and create it if it doesn't already exist
         foreach ($roles as $roleName) {
             Role::firstOrCreate(['name' => $roleName]);
         }
 
+        // Go through every user in the database
         User::all()->each(function ($user) {
+            // If the user has a 'role' attribute, assign the corresponding role
             if ($user->role) {
                 $user->assignRole($user->role);
             }
