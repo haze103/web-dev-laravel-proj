e03140d65a79581f869630ad474facfca6bcd747
Sybil <oui.cruzado@gmail.com>
2025-06-29 17:38:18 +0800

    Removed Emoticons

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Spatie\Permission\Traits\HasRoles; // ✅ Import this
+use Spatie\Permission\Traits\HasRoles; 
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles; // ✅ Add HasRoles here
+    use HasFactory, Notifiable, HasRoles; 
 
     /**
      * The attributes that are mass assignable.
