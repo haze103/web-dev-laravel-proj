9595570f315a8b24929148997a204e78cdb58ded
Sybil <oui.cruzado@gmail.com>
2025-06-29 19:48:19 +0800

    Added seeder for spatie

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,21 @@
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+
+class RoleSeeder extends Seeder
+{
+    public function run()
+    {
+        $roles = ['Super Admin', 'Admin', 'Sales Manager', 'Sales Representative'];
+
+        foreach ($roles as $roleName) {
+            Role::firstOrCreate(['name' => $roleName]);
+        }
+
+        User::all()->each(function ($user) {
+            if ($user->role) {
+                $user->assignRole($user->role);
+            }
+        });
+    }
+}
